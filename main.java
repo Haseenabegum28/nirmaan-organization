0705d38b1e397a4df9b184d96e2db57ffbd757c1
Haseenabegum28 <144517410+Haseenabegum28@users.noreply.github.com>
2025-05-23 10:18:35 +0530

    day17

---
+++ main.java
@@ -0,0 +1,24 @@
+package day17;
+import java.util.Scanner;
+
+
+public class main {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the First Number");
+		int num1 = sc.nextInt();
+		System.out.println("Enter the second Number");
+		int num2 = sc.nextInt();
+		
+		int total = num1+num2;
+		
+		
+		System.out.println("Total : "+total);
+		
+		
+		
+	}
+	
+}
