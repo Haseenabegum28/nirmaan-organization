74f29472f7a977d65d45fecaf661adcab536073a
Haseenabegum28 <144517410+Haseenabegum28@users.noreply.github.com>
2025-05-23 10:17:58 +0530

    day1

---
+++ helloworld.java
@@ -0,0 +1,25 @@
+package Day1;
+
+public class helloworld {
+public static void main(String[] args) {
+	
+    String myname="Haseena";
+    System.out.println(myname);
+    boolean myclass=false;
+    System.out.println(myclass);
+    short mynum=100;
+    System.out.println(mynum);
+    char myname1='e';
+    System.out.println(myname1);
+    char myvar1='A',myvar2='B';
+    System.out.println(myvar1);
+    System.out.println(myvar2);
+    String studname="mohan";
+    System.out.println("Student name:"+studname);
+  //arithmetic operators
+    int x=100,y=50;
+    System.out.println(x+y);
+    
+    
+}
+}
