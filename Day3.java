23b936992a6960c13b44e379c6126bfee151b850
Haseenabegum28 <144517410+Haseenabegum28@users.noreply.github.com>
2025-05-24 12:41:23 +0530

    Add files via upload

--- Day3.java
+++ Day3.java
@@ -1,15 +1,12 @@
-package votecheck;
-import java.util.Scanner;
-public class Day3 {
-	public static void main(String[]args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter your age");
-		int age=sc.nextInt();
-		if(age>18) {
-			System.out.println("you are eligible to vote");
-			}else{
-				System.out.println("you are not eligible to vote");
-			}
-	}
+package votingsystem;
 
+public class Day3 {
+	public static void main(String []args) {
+		int age=17;
+		if(age<18) {
+		System.out.println("you are eligible to vote");
+		}else{
+			System.out.println("you are not eligible to vote");
+		}
+	}
 }
