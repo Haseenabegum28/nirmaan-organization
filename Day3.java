c31aa67c1f15654b6d82351a211f632e060a37ec
Haseenabegum28 <144517410+Haseenabegum28@users.noreply.github.com>
2025-05-24 12:34:42 +0530

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,15 @@
+package condition;
+
+import java.util.Scanner;
+
+public class Day3 {
+public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter a number");
+	int i=sc.nextInt();
+	while(i<5) {
+		System.out.println(i);
+		i++;
+	}
+}
+}
