ab0114f77b71620686e571df256b084a5e1881b0
Haseenabegum28 <144517410+Haseenabegum28@users.noreply.github.com>
2025-05-24 12:40:19 +0530

    Add files via upload

--- Day3.java
+++ Day3.java
@@ -1,12 +1,16 @@
-package dowhile;
+package Studentgrading;
 
 public class Day3 {
 public static void main(String[]args) {
-	int i=0;
-	do {
-		System.out.println(i);
-		i++;
+	int mark=85;
+	if(mark<35) {
+		System.out.println("Fail");
+	}else if(mark>=90) {
+		System.out.println("Grade A");
+	}else if(mark>=80) {
+		System.out.println("Grade B");
+	}else {
+		System.out.println("Grade C");
 	}
-	while(i<5);
 }
 }
